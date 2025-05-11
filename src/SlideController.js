ed3520e88157f4dcfa6261265563951b4df5d02c
Saša Savić <sasa@savic.one>
2025-05-11 13:36:32 +0200

    feat: Wrap up slide controller

--- src/SlideController.js
+++ src/SlideController.js
@@ -1,18 +1,14 @@
 import { Children } from 'react';
 
 export default function SlideController({children}) {
-  // Change the background color of every other child
-  console.log(children, typeof children);
-
-  let all_children = Children.toArray(children);
+  let all_children = Children.toArray(children)
   let to_render = all_children.map((child, i) => {
-    let bg = i % 2 === 0 ? "bg-white" : "bg-blue";
     return (
-      <div key={i} className={bg}>
+      <div key={i}>
         {child}
       </div>
     )
-  });
+  })
 
   return (
     <div className="wrap">
--- src/slides/Disclaimer.js
+++ src/slides/Disclaimer.js
@@ -1,5 +1,14 @@
 export default function DisclaimerSlide() {
+  let children = ["Disclaimer", "Disclaimer", "Disclaimer"]
+
   return (
-      <h1 className="text-6xl ">Disclaimer</h1>
-  );
+    <section>
+      {children.map((text, i ) => {
+        let rotate_180 = i  % 2 === 0 ? "transform -scale-y-50" : ""
+        return (
+          <h1 key={i} className={"text-6xl " + rotate_180}>{text}</h1>
+        )
+      })}
+    </section>
+  )
 }
