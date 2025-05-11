ddbd852e3477773aff362f07be83918752b0ceff
Saša Savić <sasa@savic.one>
2025-05-11 12:44:11 +0200

    refactor: move react app to root

--- src/SlideController.js
+++ src/SlideController.js
@@ -1,10 +1,14 @@
+import { Children } from 'react';
+
 export default function SlideController({children}) {
   // Change the background color of every other child
   console.log(children, typeof children);
-  let to_render = children.map((child, index) => {
-    let bg = index % 2 === 0 ? "bg-white" : "bg-blue";
+
+  let all_children = Children.toArray(children);
+  let to_render = all_children.map((child, i) => {
+    let bg = i % 2 === 0 ? "bg-white" : "bg-blue";
     return (
-      <div className="{bg}">
+      <div key={i} className={bg}>
         {child}
       </div>
     )
